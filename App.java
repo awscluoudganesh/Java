4a208fa46891ad08e18bb5f11f3b3e474923808b
awscluoudganesh <awscluoud.ganesh@gmail.com>
2025-03-05 08:12:55 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World! , This is Shrii Ganesh !!");
+        System.out.println("Hello, World! , This is Shrii Ganesh , Nice to meet you!!");
     }
 }
